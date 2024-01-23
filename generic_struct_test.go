6309328e8cd69b90549fa467bf9b205ca28fbc1c
MuhammadIbraAlfathar <ibraalfathar31@gmail.com>
2024-01-23 13:30:09 +0700

    add generic in method

--- generic_struct_test.go
+++ generic_struct_test.go
@@ -2,12 +2,22 @@ package go_generics
 
 import (
 	"fmt"
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 type Product[T any] struct {
 	Name T
 	Id   T
+}
+
+func (p *Product[T]) SayHello(name string) string {
+	return "Hello " + name
+}
+
+func (p *Product[T]) ChangeName(name T) T {
+	p.Name = name
+	return p.Name
 }
 
 func TestGenericStruct(t *testing.T) {
@@ -17,4 +27,14 @@ func TestGenericStruct(t *testing.T) {
 	}
 
 	fmt.Println(data)
+}
+
+func TestGenericMethod(t *testing.T) {
+	data := Product[string]{
+		Name: "Phone",
+		Id:   "112233",
+	}
+
+	assert.Equal(t, "Fashion", data.ChangeName("Fashion"))
+	assert.Equal(t, "Hello Ibra", data.SayHello("Ibra"))
 }
