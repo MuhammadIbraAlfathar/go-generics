5a3ca9b773eaf6422e17b90207c1cdd1326c9a67
MuhammadIbraAlfathar <ibraalfathar31@gmail.com>
2024-01-23 13:42:36 +0700

    add generic in interface

---
+++ generic_interface_test.go
@@ -0,0 +1,37 @@
+package go_generics
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+type GetterSetter[T any] interface {
+	GetValue() T
+	SetValue(value T)
+}
+
+func ChangeValue[T any](param GetterSetter[T], value T) T {
+	param.SetValue(value)
+	return param.GetValue()
+}
+
+type MyData[T any] struct {
+	Value T
+}
+
+func (d *MyData[T]) GetValue() T {
+	return d.Value
+}
+
+func (d *MyData[T]) SetValue(value T) {
+	d.Value = value
+}
+
+func TestGenericInterface(t *testing.T) {
+	data := MyData[string]{}
+
+	result := ChangeValue(&data, "OK")
+
+	assert.Equal(t, "OK", result)
+	assert.Equal(t, "OK", data.Value)
+}
